48723d87f1132ac90f1f93c22cc269bdf44a1146
Goha20012 <125872900+Goha20012@users.noreply.github.com>
2024-02-02 01:01:45 +0300

    first commit

---
+++ Lab3/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxTotalSum = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnCreateFiles = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxTotalSum
+            // 
+            this.textBoxTotalSum.Location = new System.Drawing.Point(321, 269);
+            this.textBoxTotalSum.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxTotalSum.Name = "textBoxTotalSum";
+            this.textBoxTotalSum.Size = new System.Drawing.Size(68, 20);
+            this.textBoxTotalSum.TabIndex = 28;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(303, 16);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(68, 20);
+            this.textBox2.TabIndex = 27;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(385, 16);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(135, 20);
+            this.dateTimePicker1.TabIndex = 26;
+            // 
+            // btnCreateFiles
+            // 
+            this.btnCreateFiles.Location = new System.Drawing.Point(33, 265);
+            this.btnCreateFiles.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCreateFiles.Name = "btnCreateFiles";
+            this.btnCreateFiles.Size = new System.Drawing.Size(165, 27);
+            this.btnCreateFiles.TabIndex = 25;
+            this.btnCreateFiles.Text = "Открыть в ворд и в эксель";
+            this.btnCreateFiles.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(33, 116);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(356, 138);
+            this.dataGridView1.TabIndex = 24;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(363, 278);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 23;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(269, 271);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "итого";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(110, 89);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(97, 20);
+            this.textBox4.TabIndex = 21;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 89);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "покупатель";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(110, 54);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(97, 20);
+            this.textBox3.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 54);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "поставщик";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 19);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "дата";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(179, 16);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(68, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "расходная накладная номер";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 312);
+            this.Controls.Add(this.textBoxTotalSum);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnCreateFiles);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTotalSum;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnCreateFiles;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Lab3/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        private bool filesCreated = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeEventHandlers();
+            SetupDataGridViewColumns();
+
+            // Автоматическое заполнение полей при инициализации формы
+            textBox1.Text = "12345"; // Пример значения
+            dateTimePicker1.Value = DateTime.Now;
+            textBox3.Text = "Поставщик";
+            textBox4.Text = "Покупатель";
+
+            // Заполнение данных в DataGridView (пример данных, замените их своими)
+            dataGridView1.Rows.Add("1", "Товар1", "10", "100", "");
+            dataGridView1.Rows.Add("2", "Товар2", "5", "5550", "");
+
+            // Вычисление и вывод суммы в каждой строке DataGridView
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                UpdateTotalForRow(i);
+            }
+
+            // Вычисление и вывод итоговой суммы
+            UpdateTotals();
+        }
+
+        private void UpdateTotals()
+        {
+            decimal totalSum = 0;
+
+            // Проход по всем строкам в DataGridView и добавление суммы из столбца "Total"
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells["Total"].Value != null)
+                {
+                    totalSum += Convert.ToDecimal(row.Cells["Total"].Value);
+                }
+            }
+
+            // Вывод общей суммы в ваш элемент управления, например, в какой-то TextBox
+            textBoxTotalSum.Text = totalSum.ToString();
+        }
+
+        private void InitializeEventHandlers()
+        {
+            label1.Click += label1_Click;
+            label2.Click += label2_Click;
+            label4.Click += label4_Click;
+            label5.Click += label5_Click;
+            textBox1.TextChanged += textBox1_TextChanged;
+            textBox4.TextChanged += textBox4_TextChanged;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            Load += Form1_Load;
+            btnCreateFiles.Click += btnCreateFiles_Click;
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = dateTimePicker1.Value.ToShortDateString();
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && (e.ColumnIndex == dataGridView1.Columns["Quantity"].Index || e.ColumnIndex == dataGridView1.Columns["Price"].Index))
+            {
+                UpdateTotalForRow(e.RowIndex);
+            }
+        }
+
+        private void UpdateTotalForRow(int rowIndex)
+        {
+            object quantityValue = dataGridView1["Quantity", rowIndex].Value;
+            object priceValue = dataGridView1["Price", rowIndex].Value;
+
+            if (quantityValue != null && priceValue != null)
+            {
+                decimal quantity = Convert.ToDecimal(quantityValue);
+                decimal price = Convert.ToDecimal(priceValue);
+                decimal total = quantity * price;
+                dataGridView1["Total", rowIndex].Value = total;
+            }
+        }
+
+        private void btnCreateFiles_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("btnCreateFiles_Click called");
+
+            CreateAndFillWordFile();
+            CreateAndFillExcelFile();
+
+        }
+
+        private void CreateAndFillWordFile()
+        {
+            try
+            {
+                Word.Application wordApp = new Word.Application();
+                wordApp.Visible = true;
+                Word.Document doc = wordApp.Documents.Add();
+
+                Word.Paragraph title = doc.Paragraphs.Add();
+                title.Range.Text = "Отчет";
+                title.Range.Font.Size = 16;
+                title.Range.Font.Bold = 1;
+                title.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                title.Range.InsertParagraphAfter();
+
+                doc.Content.Text += $"Расходная накладная номер: {textBox1.Text}\n";
+                doc.Content.Text += $"Дата: {dateTimePicker1.Value.ToShortDateString()}\n";
+                doc.Content.Text += $"Поставщик: {textBox3.Text}\n";
+                doc.Content.Text += $"Покупатель: {textBox4.Text}\n";
+
+                int numRows = dataGridView1.RowCount + 1;
+                int numColumns = dataGridView1.ColumnCount;
+
+                Word.Table table = doc.Tables.Add(title.Range, numRows, numColumns);
+                table.Borders.Enable = 1;
+
+                // Заполнение заголовков таблицы
+                for (int i = 0; i < numColumns; i++)
+                {
+                    table.Cell(1, i + 1).Range.Text = dataGridView1.Columns[i].HeaderText;
+                }
+
+                // Заполнение данных в таблицу
+                for (int i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    for (int j = 0; j < numColumns; j++)
+                    {
+                        table.Cell(i + 2, j + 1).Range.Text = dataGridView1[j, i].Value?.ToString() ?? "";
+                    }
+                }
+
+                // Итоговая сумма
+                table.Cell(numRows, 1).Range.Text = "Итоговая сумма:";
+                table.Cell(numRows, 2).Range.Text = textBoxTotalSum.Text;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void CreateAndFillExcelFile()
+        {
+            try
+            {
+                Excel.Application excelApp = new Excel.Application();
+                excelApp.Visible = true;
+                Excel.Workbooks workbooks = excelApp.Workbooks;
+                Excel.Workbook workbook = workbooks.Add();
+                Excel.Worksheet worksheet = (Excel.Worksheet)workbook.Worksheets[1];
+
+                worksheet.Cells[1, 1] = "Расходная накладная номер";
+                worksheet.Cells[1, 2] = "Дата";
+                worksheet.Cells[1, 3] = "Поставщик";
+                worksheet.Cells[1, 4] = "Покупатель";
+
+                worksheet.Cells[2, 1] = textBox1.Text;
+                worksheet.Cells[2, 2] = textBox2.Text;
+                worksheet.Cells[2, 3] = textBox3.Text;
+                worksheet.Cells[2, 4] = textBox4.Text;
+
+                for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                {
+                    // Проверка на null перед присваиванием значения ячейки
+                    object headerValue = dataGridView1.Columns[i].HeaderText;
+                    worksheet.Cells[1, i + 5] = headerValue != null ? headerValue.ToString() : string.Empty;
+                }
+
+                for (int i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                    {
+                        // Проверка на null перед присваиванием значения ячейки
+                        object cellValue = dataGridView1[j, i].Value;
+                        worksheet.Cells[i + 2, j + 5] = cellValue != null ? cellValue.ToString() : string.Empty;
+                    }
+                }
+
+                // Добавление итоговой строки для столбца "Total"
+                int rowCount = dataGridView1.RowCount + 2;
+                worksheet.Cells[rowCount, 1] = "Итоговая сумма:";
+                worksheet.Cells[rowCount, 5].Formula = $"SUM(I2:I{rowCount - 1})"; // Подставьте номер столбца "Total" вместо 5
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void SetupDataGridViewColumns()
+        {
+            dataGridView1.Columns.Add("Number", "номер");
+            dataGridView1.Columns.Add("Product", "товар");
+            dataGridView1.Columns.Add("Quantity", "количество");
+            dataGridView1.Columns.Add("Price", "цена");
+            dataGridView1.Columns.Add("Total", "сумма");
+
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                UpdateTotalForRow(i);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // Обработчик события для label1
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            // Обработчик события для label2
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            // Обработчик события для label4
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            // Обработчик события для label5
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Обработчик события для textBox1
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            // Обработчик события для textBox4
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Обработчик события для dataGridView1
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Обработчик события для Form1_Load
+        }
+    }
+}
